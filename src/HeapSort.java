972033b98237bed7a7982bfbb6dab62baf160fac
wahek <wahek1999@mail.ru>
2023-05-29 19:27:45 +0300

    Initial commit

---
+++ src/HeapSort.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+
+// Java program for implementation of Heap Sort
+public class HeapSort
+{
+
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+
+        // Heap sort
+        for (int i=n-1; i>=0; i--)
+        {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Heapify root element
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int arr[], int n, int i)
+    {
+        // Find largest among root, left child and right child
+        int largest = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // Swap and continue heapifying if root is not largest
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i < n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {1,12,9,5,6,10};
+
+        HeapSort hs = new HeapSort();
+
+        int[] arr1;
+        arr1 = new int[1000];
+        fillArray(arr1);
+        hs.sort(arr1);
+        printArray(arr1);
+    }
+    static void fillArray(int[] arr){
+        Random random = new Random();
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = random.nextInt(100);
+        }
+    }
+}
